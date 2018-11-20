e1e4ca8073345be405437a84755d7c34d3b560df
sophia <sophia.zhu10@gmail.com>
2018-11-20 13:52:12 +1000

    update

--- jsobjects.js
+++ jsobjects.js
@@ -1,4 +1,4 @@
-let me = {
+/* let me = {
     age: 31,
     currentLocation: 'london',
 }
@@ -6,8 +6,12 @@ let me = {
 me.hobbies = ['singing', 'dancing', 'reading', 'surfing']
 delete me.age
 
-console.log('my hobbies are ' + `${me.hobbies}`);
+console.log('my hobbies are ' + `${me.hobbies}`); */
 
 // for (let hobbie of me.hobbies) {
 //     console.log('my hobby is ' + `${hobbie}`);
 // }
+
+let str = 'vist my home'
+let newStr = str.replace('home', 'car')
+console.log(newStr)
